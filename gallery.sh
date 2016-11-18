d09fc07cd4ad4d296233c6e823e24edc97cf48c4
lqfortytwo <lqfmickey@gmail.com>
2016-11-18 14:31:56 -0500

    echo with color

--- gallery.sh
+++ gallery.sh
@@ -2,6 +2,11 @@
 : ${SOMATIC_API_KEY?"Please set SOMATIC_API_KEY in the enviroment to make api calls."}
 STYLES=('MZJNYmZY' 'Bka9oBkM' '8k8aLmnM' 'LnL71DkK' 'LnL7oLkK' '7ZxJo3k9')
 
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
 transfer() { if [ $# -eq 0 ]; then echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
 tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }
@@ -15,7 +20,7 @@ if [ -d "$1" ] ; then
   for file in "$1"/*; do
     if [ -f "$file" ] ; then
       IMAGE="$(pwd -P)/${file##*/}"
-      echo "$IMAGE is being processed..."
+      echo -e "${BLUE}$IMAGE ${YELLOW}is being processed...${NC}"
       IMAGE_URL="$(transfer $IMAGE)"
       RANDOM=$$$(date +%s)
       STYLE_ID=${STYLES[$RANDOM % ${#STYLES[@]}]}
@@ -23,6 +28,6 @@ if [ -d "$1" ] ; then
     fi
   done
 else
-  echo "Please pass a folder name."
+  echo -e "${RED}Please pass a folder name.${NC}"
 fi
-echo "Your gallery has been generated!"
+echo -e "${GREEN}Your gallery has been generated!${NC}"
